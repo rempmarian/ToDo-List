995ff0fa80d6aa056be8c61dd6f8dddea69220b5
Marian <marian.h.rempola@gmail.com>
2020-07-13 15:59:01 -0400

    Initial Commit

---
+++ ToDo ListTests/ToDo_ListTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToDo_ListTests.swift
+//  ToDo ListTests
+//
+//  Created by Marian Rempola on 7/13/20.
+//  Copyright © 2020 Marian Rempola. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo_List
+
+class ToDo_ListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
